40676ef89293b618d4d1189cb7aa479b9b564730
Stefanos Delikaraoglou <sdelikaraoglou@gmail.com>
2019-09-23 15:24:25 +0200

    Update Lib_ObjFnct_cluster_multiprocess_Parallel.py

--- Lib_ObjFnct_cluster_multiprocess_Parallel.py
+++ Lib_ObjFnct_cluster_multiprocess_Parallel.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 01 13:37:04 2016
+For GNU GLP v3:
 
-Library of Objective Functions
+{{Data-driven Adaptive Benders Decomposition for the Stochastic Unit Commitment Problem}}
+Copyright (C) {{ 2019 }}  {{ ETH Zurich }}
 
-@author: stde
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 import gurobipy as gb
 from defaults import SchedulingHorizon,nodenb,VOLL,multicut,NScen
