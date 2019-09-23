a8258725b82709962b88e506e32ce4f2b3ebe9b7
Stefanos Delikaraoglou <sdelikaraoglou@gmail.com>
2019-09-23 15:26:56 +0200

    Update kmedoids.py

--- kmedoids.py
+++ kmedoids.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 27 11:18:39 2019
+For GNU GLP v3:
 
-@author: bvandenb
+{{Data-driven Adaptive Benders Decomposition for the Stochastic Unit Commitment Problem}}
+Copyright (C) {{ 2019 }}  {{ ETH Zurich }}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 import numpy as np
