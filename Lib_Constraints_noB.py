cf62a6d02785bb99c8d3f62a2211de2c81b882c9
Stefanos Delikaraoglou <sdelikaraoglou@gmail.com>
2019-09-23 15:24:51 +0200

    Update Lib_Constraints_noB.py

--- Lib_Constraints_noB.py
+++ Lib_Constraints_noB.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 07 19:00:51 2016
+For GNU GLP v3:
 
-@author: stde
+{{Data-driven Adaptive Benders Decomposition for the Stochastic Unit Commitment Problem}}
+Copyright (C) {{ 2019 }}  {{ ETH Zurich }}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 import gurobipy as gb
 import defaults
